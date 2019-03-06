e433f92c37e0d24947643ba406d330a60fe2a1d0
DMMunday1217 <47825413+DMMunday1217@users.noreply.github.com>
2019-03-06 10:50:11 -0600

    Update GameBoardTest.java

--- GameBoardTest.java
+++ GameBoardTest.java
@@ -1,29 +1,59 @@
 package gameBoard;
+import gameBoard.ComPlayer;
 import java.util.*;
 
-public class GameBoardTest{
+public class GameBoardTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Board playBoard = new Board();
+		boolean compPlayer = false;
 		Scanner sc = new Scanner(System.in);
-		do {
-
-			int spotToPlace = sc.nextInt();
+		System.out.println("Computer Player (y/n)? ");
+		compPlayer = (sc.next().equalsIgnoreCase("y"));
+		if (compPlayer) {
 			
-			playBoard.placePiece(spotToPlace);
-			
-			System.out.println("");
-
+			Board playBoard = new Board();
+			ComPlayer AIPlayer = new ComPlayer(true);
 			playBoard.printBoard();
+			do {
+				int spotToPlace = sc.nextInt();
 
-			System.out.println("");
+				playBoard.placePiece(spotToPlace);
 
-		} while (Checker.winTest(playBoard.returnBoard()) == 0 && playBoard.returnTurns() < 42);
+				System.out.println("");
 
-		System.out.println("PLAYER " + Checker.winTest(playBoard.returnBoard()) + " WINS!");
+				if(Checker.winTest(playBoard.returnBoard()) == 0) {
+					playBoard.placePiece(AIPlayer.placePiece(playBoard.returnBoard()));
+				}
+				
+				playBoard.printBoard();
 
+				System.out.println("");
+
+			} while (Checker.winTest(playBoard.returnBoard()) == 0 && playBoard.returnTurns() < 42);
+
+			System.out.println("PLAYER " + Checker.winTest(playBoard.returnBoard()) + " WINS!");
+			sc.close();
+			
+		} else if (!compPlayer) {
+			Board playBoard = new Board();
+			playBoard.printBoard();
+			do {
+				int spotToPlace = sc.nextInt();
+
+				playBoard.placePiece(spotToPlace);
+
+				System.out.println("");
+
+				playBoard.printBoard();
+
+				System.out.println("");
+
+			} while (Checker.winTest(playBoard.returnBoard()) == 0 && playBoard.returnTurns() < 42);
+
+			System.out.println("PLAYER " + Checker.winTest(playBoard.returnBoard()) + " WINS!");
+			sc.close();
+		}
 	}
-
 }
