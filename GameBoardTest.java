b23ad9bb37fc4418986a00e60e271d43849caa85
Luke Turczynski <43213214+Cyzynski@users.noreply.github.com>
2019-03-27 10:49:03 -0500

    Update GameBoardTest.java

--- GameBoardTest.java
+++ GameBoardTest.java
@@ -1,18 +1,17 @@
-package gameBoard;
-import gameBoard.ComPlayer;
+package connectFour;
+
+import connectFour.ComPlayer;
 import java.util.*;
 
 public class GameBoardTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		boolean compPlayer = false;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Computer Player (y/n)? ");
 		compPlayer = (sc.next().equalsIgnoreCase("y"));
 		if (compPlayer) {
-			
+
 			Board playBoard = new Board();
 			ComPlayer AIPlayer = new ComPlayer(true);
 			playBoard.printBoard();
@@ -23,10 +22,10 @@ public class GameBoardTest {
 
 				System.out.println("");
 
-				if(Checker.winTest(playBoard.returnBoard()) == 0) {
+				if (Checker.winTest(playBoard.returnBoard()) == 0) {
 					playBoard.placePiece(AIPlayer.placePiece(playBoard.returnBoard()));
 				}
-				
+
 				playBoard.printBoard();
 
 				System.out.println("");
@@ -35,7 +34,7 @@ public class GameBoardTest {
 
 			System.out.println("PLAYER " + Checker.winTest(playBoard.returnBoard()) + " WINS!");
 			sc.close();
-			
+
 		} else if (!compPlayer) {
 			Board playBoard = new Board();
 			playBoard.printBoard();
