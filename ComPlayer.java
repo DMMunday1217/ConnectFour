fd2c2cfdc4faaac67e5d19a5a973c4ba84e4e7e3
Luke Turczynski <43213214+Cyzynski@users.noreply.github.com>
2019-04-03 09:57:01 -0500

    Update ComPlayer.java

--- ComPlayer.java
+++ ComPlayer.java
@@ -9,6 +9,7 @@ import java.util.*;
  *
  */
 public class ComPlayer {
+	private static final boolean DEBUG = false;
 	/**
 	 * difficulty level. false if easy, true if hard
 	 */
@@ -46,22 +47,18 @@ public class ComPlayer {
 		Random rand = new Random();
 		if (hardMode) {
 			// piece placement for a hard bot
-			if (earlyGame() != -1) {
-				// if the middle 3 in the bottom row are open, it places it in one of those
-				// spaces, prioritizing the center one.
-				// This keeps a player from winning by putting pieces in the locations 4, 3, 2,
-				// and then 1.
-				return earlyGame();
-			} else if (findPlace() != -1) {
+			if (findPlace() != -1) {
 				// checks for 3 in a rows, if any are found, it places a piece in in the fourth
 				// spot
 				return findPlace() + 1;
-			} else if (checkGaps() != -1) {
+			} else if (checkHoroz() != -1) {
 				// in the case of a row going 1011, places a piece in the spot that is empty.
 				// This occurs regardless of which team has a piece there
-				return checkGaps() + 1;
+				return checkHoroz() + 1;
 			} else {
 				// if the bot can't find a better location, it places it like an easy bot
+				if (DEBUG)
+					System.out.println("Rand");
 				return rand.nextInt(7) + 1;
 			}
 		} else {
@@ -70,43 +67,45 @@ public class ComPlayer {
 		}
 	}
 
-	private int earlyGame() {
-		if (b[3][HEIGHT - 1] == 0) {
-			return 4;
-		} else if (b[2][HEIGHT - 1] == 0) {
-			return 3;
-		} else if (b[4][HEIGHT - 1] == 0) {
-			return 5;
-		} else {
-			return -1;
-		}
-	}
-
-	private int checkGaps() {
-		for (int y = 0; y < HEIGHT; y++) {// checks 3rd spot in a set of 4
+	private int checkHoroz() {
+		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH - 3; x++) {
-				if (b[x][y] == b[x + 1][y] && b[x][y] == b[x + 3][y] && b[x][y] != 0) {
-					if ((y == 0 || b[x + 2][y - 1] != 0) && b[x + 2][0] == 0) {
-						return x + 2;
-					}
+				if (b[x][y] == b[x + 1][y] && b[x][y] == b[x + 2][y] && b[x + 3][y] == 0 && b[x][y] != 0
+						&& ((y + 1 < HEIGHT && b[x + 3][y + 1] != 0) || y == HEIGHT - 1)) {
+					if (DEBUG)
+						System.out.println("Loc 4: " + x + ", " + y);
+					return x + 3; // returns x + 4 because it wants x + 3 but the method that places the piece
+									// subtracts one
 				}
 			}
 		}
-		for (int y = 0; y < HEIGHT; y++) {// checks 2nd spot in a set of 4
+		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH - 3; x++) {
-				if (b[x][y] == b[x + 2][y] && b[x][y] == b[x + 3][y] && b[x][y] != 0) {
-					if ((y == 0 || b[x + 1][y - 1] != 0) && b[x + 1][0] == 0) {
-						return x + 1;
-					}
+				if (b[x + 1][y] == b[x + 2][y] && b[x + 1][y] == b[x + 3][y] && b[x][y] == 0 && b[x + 1][y] != 0
+						&& ((y + 1 < HEIGHT && b[x][y + 1] != 0) || y == HEIGHT - 1)) {
+					if (DEBUG)
+						System.out.println("Loc 1: " + x + ", " + y);
+					return x;
 				}
 			}
 		}
-		for (int y = 0; y < HEIGHT; y++) {// checks 1st spot in a set of 4
+		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH - 3; x++) {
-				if (b[x + 1][y] == b[x + 2][y] && b[x][y] == b[x + 3][y] && b[x][y] != 0) {
-					if ((y == 0 || b[x][y - 1] != 0) && b[x][0] == 0){
-						return x;
-					}
+				if (b[x][y] == b[x + 2][y] && b[x][y] == b[x + 3][y] && b[x + 1][y] == 0 && b[x][y] != 0
+						&& ((y + 1 < HEIGHT && b[x + 1][y + 1] != 0) || y == HEIGHT - 1)) {
+					if (DEBUG)
+						System.out.println("Loc 2: " + x + ", " + y);
+					return x + 1;
+				}
+			}
+		}
+		for (int y = 0; y < HEIGHT; y++) {
+			for (int x = 0; x < WIDTH - 3; x++) {
+				if (b[x][y] == b[x + 1][y] && b[x][y] == b[x + 3][y] && b[x + 2][y] == 0 && b[x][y] != 0
+						&& ((y + 1 < HEIGHT && b[x + 2][y + 1] != 0) || y == HEIGHT - 1)) {
+					if (DEBUG)
+						System.out.println("Loc 3: " + x + ", " + y);
+					return x + 2;
 				}
 			}
 		}
@@ -114,59 +113,59 @@ public class ComPlayer {
 	}
 
 	private int findPlace() {
-		for (int y = 0; y < HEIGHT; y++) { // checks horizontally from top to bottom
-			for (int x = 0; x < WIDTH - 3; x++) {
-				if (b[x][y] == b[x + 1][y] && b[x][y] == b[x + 2][y] && b[x][y] != 0 && b[x + 3][y] == 0
-						&& (y + 1 < HEIGHT && b[x + 3][y + 1] == 0)) {
-					if(b[x + 3][0] == 0) {
-						return x + 3;
-					}
-				}
-			}
-		}
 		for (int y = 0; y < HEIGHT - 3; y++) { // checks vertically from left to right
 			for (int x = 0; x < WIDTH; x++) {
-				if (b[x][y] == b[x][y + 1] && b[x][y] == b[x][y + 2] && b[x][y] != 0 && b[x][y + 3] == 0) {
-					if(b[x + 3][0] == 0) {
-						return x + 1;
+				if (b[x][y + 3] == b[x][y + 1] && b[x][y + 3] == b[x][y + 2] && b[x][y] == 0 && b[x][y + 3] != 0) {
+					if (b[x][0] == 0) {
+						if (DEBUG)
+							System.out.println("Vert");
+						return x;
 					}
 				}
 			}
 		}
-		for (int y = 0; y < HEIGHT - 3; y++) { // checks diagonally left to right, top to bottom
+		for (int y = 0; y < HEIGHT - 3; y++) { // checks diagonally left to right, bottom to top
 			for (int x = 0; x < WIDTH - 3; x++) {
 				if (b[x][y] == b[x + 1][y + 1] && b[x][y] == b[x + 2][y + 2] && 0 == b[x + 3][y + 3] && b[x][y] != 0
 						&& (y + 4 < HEIGHT && b[x + 3][y + 4] == 0)) {
-					if(b[x + 3][0] == 0) {
+					if (b[x + 3][0] == 0) {
+						if (DEBUG)
+							System.out.println("Diag 1");
 						return x + 3;
 					}
 				}
 			}
 		}
-		for (int y = 0; y < HEIGHT - 3; y++) { // checks diagonally right to left, top to bottom
+		for (int y = 0; y < HEIGHT - 3; y++) { // checks diagonally right to left, bottom to top
 			for (int x = WIDTH - 1; x > WIDTH - 4; x--) {
 				if (b[x][y] == b[x - 1][y + 1] && b[x][y] == b[x - 2][y + 2] && b[x - 3][y + 3] == 0 && b[x][y] != 0) {
-					if(b[x - 3][0] == 0) {
+					if (b[x - 3][0] == 0 && b[x - 3][y + 2] != 0) {
+						if (DEBUG)
+							System.out.println("Diag 2");
 						return x - 3;
 					}
 				}
 			}
 		}
-		for (int y = HEIGHT - 1; y > HEIGHT - 3; y--) { // checks diagonally left to right, bottom to top
+		for (int y = HEIGHT - 1; y > HEIGHT - 3; y--) { // checks diagonally left to right, top to bottom
 			for (int x = 0; x < WIDTH - 3; x++) {
 				if (b[x][y] == b[x + 1][y - 1] && b[x][y] == b[x + 2][y - 2] && 0 == b[x + 3][y - 3] && b[x][y] != 0
 						&& b[x + 3][y - 2] != 0) {
-					if(b[x + 3][0] == 0) {
+					if (b[x + 3][0] == 0) {
+						if (DEBUG)
+							System.out.println("Diag 3");
 						return x + 3;
 					}
 				}
 			}
 		}
-		for (int y = HEIGHT - 1; y > HEIGHT - 3; y--) { // checks diagonally right to left, bottom to top
+		for (int y = HEIGHT - 1; y > HEIGHT - 3; y--) { // checks diagonally right to left, top to bottom
 			for (int x = WIDTH - 1; x > WIDTH - 4; x--) {
 				if (b[x][y] == b[x - 1][y - 1] && b[x][y] == b[x - 2][y - 2] && 0 == b[x - 3][y - 3] && b[x][y] != 0
 						&& b[x - 3][y - 2] != 0) {
-					if(b[x - 3][0] == 0) {
+					if (b[x - 3][0] == 0) {
+						if (DEBUG)
+							System.out.println("Diag 4");
 						return x - 3;
 					}
 				}
